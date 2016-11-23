2ba182ba7383aa8f36736d0211e5933b821a415d
prakash-bala <prakashbala.b4@gmail.com>
2016-11-23 12:05:54 +0530

    initial commit

---
+++ app/src/main/java/demo/reader/notification/notification/MainActivity.java
@@ -0,0 +1,39 @@
+package demo.reader.notification.notification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.tv);
+        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
+
+    }
+
+
+    private BroadcastReceiver onNotice= new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String pack = intent.getStringExtra("package");
+            String title = intent.getStringExtra("title");
+            String text = intent.getStringExtra("text");
+            textView.setText(pack+" "+title+" "+text);
+
+
+        }
+    };
+}
